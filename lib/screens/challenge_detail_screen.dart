024ca9c46370aa8b50f462447d6472766865bdef
jooya38 <hyj0102809@gmail.com>
2025-10-05 18:22:53 +0900

    [STYLE] : 챌린지 상세 페이지 UI 정렬 및 padding, 색상 수정

--- lib/screens/challenge_detail_screen.dart
+++ lib/screens/challenge_detail_screen.dart
@@ -23,7 +23,7 @@ class _ChallengeDetailScreen extends State<ChallengeDetailScreen> {
           color: Colors.white,
           child: ElevatedButton(
             style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xff4CAF50),
+              backgroundColor: const Color(0xFF00AA5D),
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(8),
               ),
@@ -34,50 +34,113 @@ class _ChallengeDetailScreen extends State<ChallengeDetailScreen> {
             },
             child: const Text(
               '참여하기',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const ChallengeItem(
-              icon: Icons.apartment_rounded,
-              title: '2026 우리 동네 공원 상상하기',
-              location: '서울시',
-              isTitleBox: true,
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Divider(color: Color(0x3E000000), thickness: 1.0),
-            ),
-            const Text(
-              '챌린지 정보',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 17),
-            Container(
-              child: Column(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const ChallengeItem(
+                icon: Icons.apartment_rounded,
+                title: '2026 우리 동네 공원 상상하기',
+                location: '서울시',
+                isTitleBox: true,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Divider(color: Color(0x3E000000), thickness: 1.0),
+              ),
+              SizedBox(height: 17),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   const Text(
-                    '기간',
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    '챌린지 정보',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                  Text(
-                    '2025-09-24 ~ 2026-08-19',
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.normal,
+                  SizedBox(height: 13),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '기간',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '2025-09-24 ~ 2026-08-19',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        SizedBox(height: 7),
+                        const Text(
+                          '설명',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '성북구 정릉로 77로에 새로 생길 공원에 대한 의견을 모집합니다.',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 7),
+                  SizedBox(height: 30),
                   const Text(
-                    '설명',
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    '보상',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
+                  SizedBox(height: 13),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          '1등 : 1000포인트',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        Text(
+                          '2등 : 700포인트',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        Text(
+                          '3등 : 500포인트',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 14),
                   Text(
-                    '성북구 정릉로 77로에 새로 생길 공원에 대한 의견을 모집합니다.',
+                    '*모든 수상작은 공원 설계 및 시공에 반영될 수 있습니다.*',
                     style: TextStyle(
                       fontSize: 14,
                       fontWeight: FontWeight.normal,
@@ -85,58 +148,37 @@ class _ChallengeDetailScreen extends State<ChallengeDetailScreen> {
                   ),
                 ],
               ),
-            ),
-            const Text(
-              '보상',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 13),
-            Text(
-              '1등 : 1000포인트',
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
-            ),
-            Text(
-              '2등 : 700포인트',
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
-            ),
-            Text(
-              '3등 : 500포인트',
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
-            ),
-            SizedBox(height: 14),
-            Text(
-              '*모든 수상작은 공원 설계 및 시공에 반영될 수 있습니다.*',
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
-            ),
-            SizedBox(height: 50),
-            Row(
-              children: [
-                Text(
-                  '제안',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                TextButton(
-                  onPressed: () {}, // 챌린지 제안 리스트 페이지로 이동
-                  child: Text(
-                    '+ 더보기',
-                    style: TextStyle(
-                      color: Color.fromRGBO(97, 117, 138, 1),
-                      fontSize: 16,
-                      fontWeight: FontWeight.normal,
+              SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '제안',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  TextButton(
+                    onPressed: () {}, // 챌린지 제안 리스트 페이지로 이동
+                    child: Text(
+                      '+ 더보기',
+                      style: TextStyle(
+                        color: Color.fromRGBO(97, 117, 138, 1),
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 17),
-            Row(
-              children: [
-                SizedBox(
-                  height: 30,
-                ), // 제안 글 제목 컴포넌트 넣을 곳 (1,2,3위 가로 자동 슬라이드 제공)
-              ],
-            ),
-          ],
+                ],
+              ),
+              SizedBox(height: 17),
+              Row(
+                children: [
+                  SizedBox(
+                    height: 30,
+                  ), // 제안 글 제목 컴포넌트 넣을 곳 (1,2,3위 가로 자동 슬라이드 제공)
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
