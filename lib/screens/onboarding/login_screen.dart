cb9a0685f557d25c2919b6e2c2bb63dc59f65f10
jhj04 <hj650272@gmail.com>
2025-10-07 02:46:49 +0900

    [FEAT] : 로그인화면 UI 구현

--- lib/screens/onboarding/login_screen.dart
+++ lib/screens/onboarding/login_screen.dart
@@ -8,8 +8,13 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
@@ -18,7 +23,7 @@ class _LoginScreenState extends State<LoginScreen> {
           children: [
             Image.asset(
               'assets/images/miyo_logo.png',
-              width: MediaQuery.of(context).size.width * 0.3,
+              width: width * 0.3,
             ),
             Text(
                   'MiYO',
@@ -39,10 +44,74 @@ class _LoginScreenState extends State<LoginScreen> {
                 letterSpacing: -1,
               )
             ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+            SizedBox(height: height * 0.05),
+
             SizedBox(
-              height: MediaQuery.of(context).size.height * 0.06,
-              width: MediaQuery.of(context).size.width * 0.8,
+              height: height * 0.06,
+              width: width * 0.8,
+              child: TextField(
+                controller: emailController,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Color(0xff757575),
+                  fontWeight: FontWeight.w500,
+                ),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 12, 
+                    vertical: 15
+                  ),
+                hintText: '이메일',
+                hintStyle: const TextStyle(
+                  fontSize: 15,
+                  color: Color(0xff757575),
+                ),
+                // filled: true,
+                // fillColor: Colors.white,
+                border: UnderlineInputBorder(
+                  // borderSide: const BorderSide(
+                  //     color: Color.fromRGBO(5, 150, 105, 1), 
+                  //     width: 1
+                  //     ),
+                ),
+              ),
+            ),
+          ),
+            SizedBox(height: height * 0.02),
+
+            SizedBox(
+              height: height * 0.06,
+              width: width * 0.8,
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Color(0xff757575),
+                  fontWeight: FontWeight.w500,
+                ),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 12, 
+                    vertical: 15
+                  ),
+                hintText: '비밀번호',
+                hintStyle: const TextStyle(
+                  fontSize: 15,
+                  color: Color(0xff757575),
+                ),
+                // filled: true,
+                // fillColor: Colors.white,
+                border: UnderlineInputBorder(
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: height * 0.05),
+
+            SizedBox(
+              height: height * 0.06,
+              width: width * 0.8,
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Color(0xff00AA5D),
@@ -63,6 +132,79 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
+            ),
+
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '아직 회원이 아니신가요?',
+                    style: TextStyle(
+                      color: Color(0xffACACAC),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {}, 
+                    child: Text(
+                      '회원가입 하기',
+                      style: TextStyle(
+                        color: Color(0xff00AA5D),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+                    minimumSize: Size(0, 0),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    '아이디',
+                    style: TextStyle(
+                      color: Color(0xffACACAC),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                Text(
+                  '|',
+                  style: TextStyle(
+                    color: Color(0xffACACAC),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+                    minimumSize: Size(0, 0),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    '비밀번호 찾기',
+                    style: TextStyle(
+                      color: Color(0xffACACAC),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
