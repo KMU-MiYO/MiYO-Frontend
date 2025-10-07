bf0313d9dad02aac22a1a3a3edde5c5c7cb29e8b
hanyj0317 <hyj0931@naver.com>
2025-10-07 22:48:51 +0900

    [FEAT] : 챌린지 참가 중 페이지 추가

--- lib/screens/challenge_ing_screen.dart
+++ lib/screens/challenge_ing_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:miyo/components/challenge_item.dart';
 import 'package:miyo/components/title_appbar.dart';
 
 class ChallengeIngScreen extends StatefulWidget {
@@ -14,6 +15,33 @@ class _ChallengeIngScreen extends State<ChallengeIngScreen> {
     return Scaffold(
       appBar: TitleAppbar(title: '참가 중인 챌린지', leadingType: LeadingType.close),
       backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const ChallengeItem(
+                categoryType: CategoryType.NaturePark,
+                title: "2026 우리 동네 공원 상상하기",
+                location: "서울시",
+              ),
+              const SizedBox(height: 16),
+              const ChallengeItem(
+                categoryType: CategoryType.Life,
+                title: "2026 성북구 편의시설 상상하기",
+                location: "성북구",
+              ),
+              const SizedBox(height: 16),
+              const ChallengeItem(
+                categoryType: CategoryType.EnvironSustain,
+                title: "2026 한강변 상상하기",
+                location: "서울시",
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
