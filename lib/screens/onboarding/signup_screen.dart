7a9b7ba59627fcad9778efd77e4c7c9bd41c8472
jhj04 <hj650272@gmail.com>
2025-10-08 00:00:24 +0900

    [FEAT] : 회원가입 화면 UI 구현

---
+++ lib/screens/onboarding/signup_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:miyo/components/title_appbar.dart';
+import 'package:miyo/screens/onboarding/signup_button.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    scrolledUnderElevation: 0;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: TitleAppbar(title: '회원가입', leadingType: LeadingType.back),
+      body: Center(
+        child: Column(
+          children: [
+            SignupButton()
+          ],
+        ),
+      ),
+    );
+  }
+}
