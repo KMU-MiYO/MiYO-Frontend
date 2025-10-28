64cad8551537ea04d365d54f3466490c6a0cfb86
jooya38 <hyj0102809@gmail.com>
2025-10-29 02:46:26 +0900

    [FIX] : main.dart 초기화면으로 설정

--- lib/screens/imaginary_map/suggestion_detail_screen.dart
+++ lib/screens/imaginary_map/suggestion_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:miyo/components/title_appbar.dart';
+import 'package:miyo/screens/imaginary_map/comment_bottom_sheet.dart';
 
 class SuggestionDetailScreen extends StatefulWidget {
   const SuggestionDetailScreen({super.key});
@@ -13,7 +14,12 @@ class _SuggestionDetailScreenState extends State<SuggestionDetailScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TitleAppbar(title: '제안 상세보기', leadingType: LeadingType.back),
-      body: Container(color: Colors.white),
+      body: Stack(
+        children: [
+          Container(color: Colors.white),
+          CommentBottomSheet(),
+        ],
+      ),
     );
   }
 }
