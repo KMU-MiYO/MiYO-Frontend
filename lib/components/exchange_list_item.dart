eb5bf191c2e52e2ef96029d443783a3db03d60b8
jooya38 <hyj0102809@gmail.com>
2025-10-29 19:05:34 +0900

    [FEAT] : 교환소 컴포넌트 생성

---
+++ lib/components/exchange_list_item.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class ExchangeListItem extends StatelessWidget {
+  final String localCachePrice;
+  final String pointPrice;
+  final VoidCallback? onTap;
+
+  const ExchangeListItem({
+    super.key,
+    required this.localCachePrice,
+    required this.pointPrice,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '지역 화폐 $localCachePrice권',
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Row(
+                children: [
+                  Image.asset('assets/images/pointIcon.png'),
+                  const SizedBox(width: 6),
+                  Text(
+                    pointPrice,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          ElevatedButton(
+            onPressed: onTap ?? () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xff00AA5D),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            child: const Text(
+              '교환 하기',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
